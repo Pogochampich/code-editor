a57dc421139515ec22aa55faca58f96ca13ee796
Kate <myworkk09@gmail.com>
2023-10-18 16:44:03 +0300

    ddd

--- src/pages/CreateTasks.jsx
+++ src/pages/CreateTasks.jsx
@@ -1,12 +1,12 @@
 const CreateTasks = () => {
-   /*  const [title, setEmail] = React.useState('');
+    const [title, setEmail] = React.useState('');
     const [result, setResult] = React.useState('');
     const [description, setDescription] = React.useState('');
 
    const createSubmit = (e: React.FormEvent) => {
         e.preventDefault();
-        api.login(email, password);
-      };*/
+        api.login(title, result, description);
+      };
     
     return (
         <section className="createForm">
@@ -14,10 +14,10 @@ const CreateTasks = () => {
                 <h1 className="createForm__h1"> Create Task</h1>
                     <div className="createForm__block">
                         <img src="../src/assets/images/createTask.png" alt=""  className="createForm__image"/>
-                        <form action="" className="createForm__form">
-                            <input type="text" className="createForm__title" placeholder="Title"/>
-                            <textarea type="text" className="createForm__description" placeholder="Description" rows={4} cols={40}/>
-                            <textarea type="text" className="createForm__Form" placeholder="Right answer" rows={4} cols={40}/>
+                        <form onSubmit={createSubmit} className="createForm__form">
+                            <input type="text" className="createForm__title" id="title" placeholder="Title"/>
+                            <textarea type="text" className="createForm__description" placeholder="Description" id="description" rows={4} cols={40}/>
+                            <textarea type="text" className="createForm__Form" placeholder="Right answer" id="result" rows={4} cols={40}/>
 
                             <button className="createForm__Button">Upload tasks</button>
                         </form>
