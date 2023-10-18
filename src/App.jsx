b529d46cda210f48a8ea4667ccab850f651b05b0
Igor Makienko <ismakienko@gmail.com>
2023-10-18 14:00:53 +0300

    basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom"
+import LoginPage from './pages/Login'
+import RegistrationPage from './pages/Registration'
+
 function App() {
 
   return (
-    <div>
-     
-    </div>
+    <>
+     <BrowserRouter>
+       <Routes>
+           <Route path="/" element={<LoginPage />} />
+           <Route path="/register" element={<RegistrationPage />} />
+       </Routes>
+     </BrowserRouter>
+    </>
   )
 }
 
